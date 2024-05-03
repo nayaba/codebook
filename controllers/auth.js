ed718ce59ed5f4b9bec132c30a5465092e752203
Nabila Ayaba <nabilaayaba@gmail.com>
2024-05-03 16:18:42 -0400

    Create user in DB with hashed password via bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,33 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
+const User = require("../models/user")
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-    res.send('Welcome to auth')
+router.get("/", (req, res) => {
+  res.send("Welcome to auth")
 })
 
-router.get('/signup', (req, res) => {
-    res.render('auth/signup.ejs')
+router.get("/signup", (req, res) => {
+  res.render("auth/signup.ejs")
+})
+
+router.post("/signup", async (req, res) => {
+  let userExists = await User.findOne({ username: req.body.username })
+  console.log('userExists: ', userExists)
+  if (userExists) {
+    return res.send("Username already taken.")
+  } else {
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.send("Password and Confirm Password must match!")
+    } else {
+      const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+      req.body.password = hashedPassword
+
+      let newUser = await User.create(req.body)
+      console.log(newUser)
+      res.send(`Thanks for signing up ${newUser.username}`)
+    }
+  }
 })
 
 module.exports = router
